356ec9203770ce62a5397cd8aca82ff2bad7a567
zcoder <zcoder@zcoderdeMacBook-Pro.local>
2021-08-30 15:11:44 +0800

    Node服务端添加容错处理中间件

--- gulls-server/app.js
+++ gulls-server/app.js
@@ -1,11 +1,13 @@
-const Koa = require('koa');
+const Koa = require("koa");
 
-const config = require('./config');
-const initController = require('./controllers/index');
+const config = require("./config");
+const initController = require("./controllers/index");
+const ErrorHandler = require("./middleware/ErrorHandler");
 
 const app = new Koa();
 
+ErrorHandler.error(app);
 initController(app);
 app.listen(config.port, () => {
   console.log(`server is running at http://localhost:${config.port}`);
-})
+});
---
+++ gulls-server/middleware/ErrorHandler.js
@@ -0,0 +1,16 @@
+class ErrorHandler {
+  static error(app) {
+    app.use(async (ctx, next) => {
+      try {
+        await next();
+        if (ctx.status === 404) {
+          ctx.body = "404页面";
+        }
+      } catch {
+        ctx.body = "500 请求，正在处理";
+      }
+    });
+  }
+}
+
+module.exports = ErrorHandler;
